bce0e39fc3c59a957ef30440449d71dfff04bb98
Nicolas Grimault <nicolas.g21@live.fr>
2018-12-03 10:47:59 +0100

    Add swagger ui

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 4300;
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database('app/data/sqlitedb');
@@ -8,6 +10,8 @@ var bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 require('./app/routes')(app, db);
 
